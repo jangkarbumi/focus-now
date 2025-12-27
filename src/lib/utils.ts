9a8d9e185f042bb083f2d70cc64915a38463d23e
jangkarbumi <wahyuuajigtn13@gmail.com>
2025-12-27 14:58:57 +0700

    Create date formatting function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatShortDate(timestamp: number): string {
+  return new Intl.DateTimeFormat('en-EN', {
+    day: "numeric",
+    month: "short",
+    year: "numeric"
+  }).format(new Date(timestamp))
+}
